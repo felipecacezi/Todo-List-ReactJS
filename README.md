13f153bb48402e03d325fde59ca88a88d5ef0275
Felipe Gustavo Teixeira da Silva <31870057+felipecacezi@users.noreply.github.com>
2021-10-05 11:45:00 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Todo-List-ReactJS
-Praticando o React JS - Todo list
+# To-do List
+
+This project made for praticing ReactJS and Typescript fundamentals.
+
+## Possible Future Improvements
+
+1.  Link a Database [ Node, Postgre Sql or SQLite ]
+2.  Create a App in React Native
+3.  Create a App in Electron
---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,27 @@
+import { Container } from './styles';
+import { Item } from '../../types/Item';
+import { useState } from 'react';
+
+
+type Props = {
+    item: Item,
+    onChange: (id:number, done:boolean) => void
+}
+
+
+export const ListItem = ({item, onChange }:Props)=>{
+
+    const [isChecked, setIsChecked] = useState(item.done);
+
+    return(
+        <Container done={item.done}>
+
+            <input type="checkbox" 
+                   checked={item.done}
+                   onChange={ e => onChange(item.id, e.target.checked) } />
+
+            <label>{item.name} - {item.done.toString()}</label>
+
+        </Container>
+    );
+}
