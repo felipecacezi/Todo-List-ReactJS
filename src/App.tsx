3d4c9cf41c945c7773cca930f0baaf564883d4ae
felipe <felipecacezi@gmail.com>
2021-10-05 11:12:39 -0300

    'projeto_todo_list_com_desafio_feito'

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Container, Area, Header } from './App.styles';
+import { Item } from './types/Item';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { ListItem }  from './components/ListItem';
+import { AddArea } from './components/AddArea';
+
+const App = ()=>{
+
+  const [list, setList] = useState<Item[]>([
+                                              { id: 1, name: 'comprar o pão na padaria', done: false },
+                                              { id: 2, name: 'comprar um bolo na padaria', done: true }
+                                            ]);
+
+
+  const handleAddTask = (taskName: string)=>{
+    
+    let newList = [...list];
+
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+
+    setList(newList);
+
+  }  
+  
+  
+  const handleTaskChange = (id:number, done:boolean)=>{
+
+    let newList = [...list];
+    
+    for(let i in newList){
+
+      if(newList[i].id === id){
+
+        newList[i].done = done;
+
+      }
+
+    }
+
+    setList(newList);
+    
+  }
+
+  return(
+    <Container>
+      <Area>
+        
+        <Header>Lista de Tarefas</Header>
+
+        <AddArea onEnter={handleAddTask}/>
+
+        {
+          list.map((item, index)=>(
+            <ListItem item={item} 
+                      key={index}
+                      onChange={handleTaskChange} />            
+          ))
+        }
+
+      </Area>
+    </Container>
   );
 }
 
